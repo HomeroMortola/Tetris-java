5b9b153c15f10b318afc158033f2b257079144b1
HomeroMortola <homemortola@gmail.com>
2025-09-10 15:14:48 -0300

    Entorno preparado

---
+++ src/test/java/com/mycompany/app/TetrisTest.java
@@ -0,0 +1,19 @@
+package com.mycompany.app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+
+public class TetrisTest {
+    public TetrisTest() {
+    }
+
+    @Test
+    public void testTetrisExiste() {
+        Tetris t = new Tetris();
+        assertNotNull(t);
+    }
+    
+}
