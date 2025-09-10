016cec5c7c58ffdb3e515b1bfe79297577472b3e
HomeroMortola <homemortola@gmail.com>
2025-09-10 15:35:59 -0300

    pice 4 elementos

--- src/main/java/com/mycompany/app/Piece.java
+++ src/main/java/com/mycompany/app/Piece.java
@@ -1,5 +1,16 @@
 package com.mycompany.app;
 
 public class Piece {
+
+    protected int[][] elementos = new int[4][4];
     
+    public int[][] getElementos() {
+        return elementos;
+    }
+
+    public int getElementos(int x, int y) {
+        return this.elementos[x][y];
+    }
+        
+        
 }
