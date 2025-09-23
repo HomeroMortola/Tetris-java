c9f8d62bc70903e53cae0e1f58c0778c03ef20ac
HomeroMortola <homeomortola@gmail.com>
2025-09-23 12:49:10 -0300

    fix pieza LL

--- src/main/java/com/mycompany/app/PieceLL.java
+++ src/main/java/com/mycompany/app/PieceLL.java
@@ -6,7 +6,7 @@ public class PieceLL extends PieceBase {
             new Cell(1, 0),
             new Cell(0, 0),
             new Cell(- 1, 0),
-            new Cell(- 2, 1)
+            new Cell(- 1, 1)
         );
     }
     
