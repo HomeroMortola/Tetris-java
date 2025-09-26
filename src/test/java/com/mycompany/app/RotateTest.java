5b7391602dc9f84984a32f7a93b0e876d0fbcfbc
ILAIGG <lucasmordka9@gmail.com>
2025-09-26 09:46:20 -0300

    Test de colisiones de piezas

--- src/test/java/com/mycompany/app/RotateTest.java
+++ src/test/java/com/mycompany/app/RotateTest.java
@@ -40,4 +40,37 @@ public class RotateTest {
         assertTrue(((Cell) c[3]).getX()==-1);
         assertTrue(((Cell) c[3]).getY()==0);
     }
+
+    @Test
+    public void testCollisionOnRotateLeft() {
+        Board board = new Board(10, 20);
+        PieceBase piece = new PieceT();
+        board.addPiece(piece);
+
+        //Crea una sola celda que ocupe la posición donde la pieza rotaría
+        int[][] grid = board.getGrid();
+        //Coloca la celda a la izquierda de la pieza (posición donde la celda [0] de PieceT estaría tras rotar a la izquierda)
+        int x = board.getCurrentPieceX() + 0;
+        int y = board.getCurrentPieceY() - 1;
+        grid[x][y] = 1; //Ocupa la celda en el grid
+
+        boolean canRotate = board.canRotate(piece, true);
+        assertTrue("La pieza NO debe poder rotar por colisión", !canRotate); //La pieza no se podría rotar debido a la colisión
+    }
+
+    //Lo mismo que el test anterior, pero para la rotación a la derecha
+    @Test
+    public void testCollisionOnRotateRight() {
+        Board board = new Board(10, 20);
+        PieceBase piece = new PieceT();
+        board.addPiece(piece);
+
+        int[][] grid = board.getGrid();
+        int x = board.getCurrentPieceX() + 0;
+        int y = board.getCurrentPieceY() + 1;
+        grid[x][y] = 1;
+
+        boolean canRotate = board.canRotate(piece, false);
+        assertTrue(!canRotate);
+    }
 }
