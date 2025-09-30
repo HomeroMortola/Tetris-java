6e37c77fdf80e11a7c94c009f622edfbb7c2ebe4
ILAIGG <lucasmordka9@gmail.com>
2025-09-29 21:39:23 -0300

    Comentarios para Clock

--- src/main/java/com/mycompany/app/Clock.java
+++ src/main/java/com/mycompany/app/Clock.java
@@ -2,16 +2,23 @@ package com.mycompany.app;
 
 public class Clock {
     private int time;
+
+    //Constructor de Clock
     public Clock() {
         setTime(0);
     }
+
+    //Set para doble encapsulamiento
     public void setTime(int time) {
         this.time = time;
     }
 
+    //Get para doble encapsulamiento
     public int getTime() {
         return time;
     }
+    
+    //Avanza el reloj un tick
     public int tick() {
         return time++;
     }
